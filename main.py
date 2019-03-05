987aa68e01785bde7dd90922c071cf458f64e6d2
James Jung <thejcode24@gmail.com>
2019-03-04 20:03:08 -0800

    added registration and login form validation for user input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -8,6 +9,12 @@ app = Flask(__name__)
 # import secrets
 # secrets.token_hex(16) creates a random 16 digit value
 app.config["SECRET_KEY"] = "2d971c13806bda77a4c3060fd5e89866"
+
+# sqlite:///site.db is the relative filepath
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+# Create SQLAlchemy database instance
+db = SQLAlchemy(app)
 
 # "dummy" data representing a blog post
 posts = [
@@ -34,16 +41,28 @@ def home():
 def about():
     return render_template("about.html", )
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     # Create instance of Registration form.
     form = RegistrationForm()
+
+    # Check form for valid info in registration form
+    if form.validate_on_submit():
+        # Use flash() to show a quick message on-screen
+        flash(f"Account Succesfully Created for {form.username.data}!", "success")
+        # After successful creation of login, show home page
+        return redirect(url_for("home"))
     return render_template("register.html", title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # Create instance of Login form.
     form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == "admin@home.gg" and form.password.data == "123":
+            flash(f"You are now logged in as {form.email.data}", "success")
+            return redirect(url_for("home"))
     return render_template("login.html", title="Login", form=form)
 
 
